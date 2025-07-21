c7a34b2e0f6f9634b3afba8c81336713343d2cd2
Piyush Kumar Raikwar <piyushraikwar289@gmail.com>
2025-07-21 17:25:38 +0530

    Login With Email or Phone Number

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -6,6 +6,12 @@ use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Jetstream\Jetstream;
 
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Fortify;
+
 class JetstreamServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +30,20 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
+
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->login)
+                ->orWhere('phone', $request->login)->
+                first();
+
+            if (
+                $user &&
+                Hash::check($request->password, $user->password)
+            ) {
+                return $user;
+            }
+        });
     }
 
     /**
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,20 +6,13 @@
             <p class="loade-text" style="font-size: 35px; font-weight: 900;"
                 data-text="{{ config('app.name', 'Laravel') }}">{{ config('app.name', 'Laravel') }}</p>
         </x-slot>
-
         <x-validation-errors class="mb-4" />
-
         @session('status')
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ $value }}
             </div>
         @endsession
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            {{-- <div style="display: flex; flex-direction: column; justify-content: center; gap:20px; text-align: center;">
-
+         {{-- <div style="display: flex; flex-direction: column; justify-content: center; gap:20px; text-align: center;">
                 <a href="{{ route('google.login') }}" class="border border-dark rounded-3 px-3 py-3"
                     style="display:flex; border-radius: 18px; flex-direction: row; justify-content: center; align-items: center;">
                     <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo"
@@ -27,40 +20,39 @@
                     <span class="flex-grow-1 text-center">Login with Google</span>
                 </a>
                 <h4 style="font-weight: 800;">OR</h4>
-
             </div> --}}
-
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
             <div
                 style="display: flex; flex-direction: column; justify-content: center; gap:20px; text-align: center; margin-bottom: 5px;">
                 <a href="{{ route('otp.login.form') }}" class="border border-dark rounded-3 px-3 py-3"
                     style="display:flex; border-radius: 18px; flex-direction: row; justify-content: center; align-items: center;">
                     <span style="font-size: 18px; background: #fff;  border-radius: 50%; margin-right: 10px;;">📞</span>
-                    <span class="flex-grow-1 text-center">Login with Phone Number</span>
+                    <span class="flex-grow-1 text-center">Login with OTP</span>
                 </a>
                 <h4 style="font-weight: 800;">OR</h4>
             </div>
-
-
-
-            <div>
+            {{-- <div>
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
                     required autofocus autocomplete="username" />
+            </div> --}}
+            <div>
+                <x-label for="login" value="{{ __('Phone Number / Email') }}" />
+                <x-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')"
+                    required autofocus autocomplete="username" />
             </div>
-
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="current-password" />
             </div>
-
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
                 {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -68,17 +60,10 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif --}}
-
                 <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
             </div>
-
-
-
-
-
-
             <div style="display: flex; flex-direction: column; justify-content: center; margin-top: 20px;">
                 <hr>
                 <br>
@@ -88,8 +73,6 @@
                     Create a New Account
                 </a>
             </div>
-
-
         </form>
     </x-authentication-card>
 </x-guest-layout>
